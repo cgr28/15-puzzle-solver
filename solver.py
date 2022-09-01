d080b69cb29802f60a77d911f6c697620c285d56
Colbe Roberson <cgr28@njit.edu>
2022-09-01 01:45:09 -0400

    new features

--- solver.py
+++ solver.py
@@ -3,6 +3,7 @@
 from puzzle import Puzzle
 from helpers import *
 from enums import *
+import sys
 
 # the puzzle that will be solved
 # edit to contain the puzzle you want solved
@@ -11,15 +12,30 @@ PUZZLE = [["1", "2", "3", "4"],
           ["9", "10", "11", "12"],
           ["13", "14", "15", "_"]]
 
-puzzle = Puzzle(PUZZLE)
+def display_solution_path(solution):
+    for i in range(len(solution)):
+        print(f"STEP {i+1} of {len(solution)}")
+        print("move: ", Translators.move_to_string(solution[i].move))
+        solution[i].puzzle.display()
+        print()
 
-# uncomment below to have puzzle shuffled automatically
-# puzzle.shuffle(100)
+if __name__ == '__main__':
+    solver_type = "bfs"
+    num_of_shuffles = 0
 
-solution = puzzle.astar()
+    if len(sys.argv) > 1:
+        solver_type = sys.argv[1]
+    if len(sys.argv) > 2:
+        num_of_shuffles = int(sys.argv[2])
 
-for i in range(len(solution)):
-    print(f"STEP {i+1} of {len(solution)}")
-    print("move: ", solution[i].move)
-    solution[i].puzzle.display()
-    print()
+    puzzle = Puzzle(PUZZLE)
+    puzzle.shuffle(num_of_shuffles)
+
+    if solver_type == "astar":
+        solution = puzzle.astar()
+    elif solver_type == "idastar":
+        solution = puzzle.idastar()
+    else:
+        solution = puzzle.best_first_search()
+    
+    display_solution_path(solution)
